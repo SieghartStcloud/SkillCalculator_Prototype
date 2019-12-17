4d4915c48955b959c45aef2161dff1d5049dfbae
Mel <ghostergameak@gmail.com>
2019-12-16 19:17:29 -0900

    add 2nd box

--- src/App.js
+++ src/App.js
@@ -51,7 +51,9 @@ function App() {
               <h1>Ghost Box</h1>
               {/* TRY SOME STUFF HERE */}
               
-              <div style={{width:'100px',height:'50px',border:'2px solid black',color:'red'}}>
+              <div style={{width:'50px',height:'25px',border:'2px solid black',color:'red',backgroundColor:'white'}}>
+              </div>
+              <div style={{width:'50px',height:'25px',border:'2px solid black',color:'blue',backgroundColor:'white"'}}>
               </div>
           </div>
               </div>
